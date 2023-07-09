8bab560f4bec8b3243e31431912fa35410a4902f
秦鹏飞 <13124026+q18539983887@user.noreply.gitee.com>
2023-07-09 17:07:46 +0800

    店铺入驻：文件上传2

--- pethome_Server/src/main/java/com/qpf/PethomeServerApplication.java
+++ pethome_Server/src/main/java/com/qpf/PethomeServerApplication.java
@@ -2,6 +2,7 @@ package com.qpf;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
